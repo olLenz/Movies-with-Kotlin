bb1a7e9a8a6ae5535ae8cd2b2667e2e78851a566
Oliver Lenz <ol.lenz@t-online.de>
2018-04-15 14:31:16 +0200

    Send device language on every request to tmdb

--- app/src/main/java/com/lenz/oliver/movieswithkotlin/repository/interceptors/AuthInterceptor.kt
+++ app/src/main/java/com/lenz/oliver/movieswithkotlin/repository/interceptors/AuthInterceptor.kt
@@ -3,8 +3,10 @@ package com.lenz.oliver.movieswithkotlin.repository.interceptors
 import com.lenz.oliver.movieswithkotlin.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
+import java.util.*
 
 private const val API_KEY = "api_key"
+private const val LANUAGE = "language"
 
 class AuthInterceptor: Interceptor {
 
@@ -14,6 +16,7 @@ class AuthInterceptor: Interceptor {
                 .url()
                 .newBuilder()
                 .addQueryParameter(API_KEY, BuildConfig.ApiKey)
+                .addQueryParameter(LANUAGE, Locale.getDefault().language)
                 .build()
         request = request.newBuilder().url(url).build()
         return chain.proceed(request)
