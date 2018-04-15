cb022602cb90136a42eb4cb2c629f89b3215f0e1
Oliver Lenz <ol.lenz@t-online.de>
2018-04-15 14:30:19 +0200

    Cross fade loaded images

--- app/src/main/java/com/lenz/oliver/movieswithkotlin/extensions.kt
+++ app/src/main/java/com/lenz/oliver/movieswithkotlin/extensions.kt
@@ -1,11 +1,13 @@
 package com.lenz.oliver.movieswithkotlin
 
 import android.widget.ImageView
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.bumptech.glide.request.target.Target
 
 fun ImageView.loadImage(imageUrl: String) {
     GlideApp.with(context)
             .load(imageUrl)
+            .transition(DrawableTransitionOptions.withCrossFade())
             .override(Target.SIZE_ORIGINAL)
             .into(this)
 }
